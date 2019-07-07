1268c024711a78414a401aa3c1b13d508aae1f78
kurehajime <xiidec@gmail.com>
2019-07-07 16:16:04 +0900

    tekari

--- src/components/BackGround.vue
+++ src/components/BackGround.vue
@@ -1,10 +1,11 @@
 <script>
 let canvas = document.createElement("canvas");
+const CANV_SIZE = 1000;
+
 /**
  * 盤面を描画してCANVASを返す。
  */
 function drawBoard(element, canvas) {
-  const CANV_SIZE = 1000;
   const COLOR_LINE = "#333333";
   const COLOR_PANEL_1 = "#660033";
   const COLOR_PANEL_2 = "#004466";
@@ -12,6 +13,7 @@ function drawBoard(element, canvas) {
   const COLOR_PANEL_4 = "#111111";
   const COLOR_PANEL_5 = "#444444";
   const COLOR_PANEL_6 = "#888888";
+  const COLOR_WHITE = "#FFFFFF";
   let cellSize = CANV_SIZE / 6;
   canvas.width = 1000;
   canvas.height = 1000;
@@ -44,7 +46,26 @@ function drawBoard(element, canvas) {
   }
   element.setAttribute("href", canvas.toDataURL());
 }
+function drawBoard2(element, canvas) {
+  const COLOR_WHITE = "#FFFFFF";
+  let cellSize = CANV_SIZE / 6;
+  let ctx_board2 = canvas.getContext("2d");
+  ctx_board2.clearRect(0, 0, CANV_SIZE, CANV_SIZE);
+  ctx_board2.globalAlpha = 0.07;
+  ctx_board2.fillStyle = COLOR_WHITE;
+  ctx_board2.beginPath();
+  ctx_board2.arc(
+    cellSize * 1,
+    -3 * cellSize,
+    7 * cellSize,
+    0,
+    Math.PI * 2,
+    false
+  );
+  ctx_board2.fill();
 
+  element.setAttribute("href", canvas.toDataURL());
+}
 export default {
   data() {
     return {};
@@ -56,12 +77,16 @@ export default {
     h: Number
   },
   mounted() {
-    drawBoard(this.$el, canvas);
+    drawBoard(this.$el.querySelector("#bg1"), canvas);
+    drawBoard2(this.$el.querySelector("#bg2"), canvas);
   }
 };
 </script>
 <template>
-  <image :x="x" :y="y" :width="w" :height="h" />
+  <g>
+    <image id="bg1" :x="x" :y="y" :width="w" :height="h" />
+    <image id="bg2" :x="x" :y="y" :width="w" :height="h" />
+  </g>
 </template>
 <style scoped>
 </style>
